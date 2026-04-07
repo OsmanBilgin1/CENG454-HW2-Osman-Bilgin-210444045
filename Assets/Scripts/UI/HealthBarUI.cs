defbe6d67ce4c876b1b8c0c77ba8dbe600ce684e
OsmanBilgin1 <efman_su@hotmail.com>
2026-04-07 13:55:58 +0300

    Healthbar ui is created and its script is connected
    feat: Healthbar ui and its script implemented as base
    (After missile tasks it will be implemented)

---
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -0,0 +1,11 @@
+// HealthBarUI.cs 
+// CENG 454 - HW2 Midterm: Sky-High Prototype II 
+// Author: Osman Bilgin | Student ID: 210444045
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarUI : MonoBehaviour
+{
+    // it is ready to implementation. After completing missile tasks this will be implemented.
+}
